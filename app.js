f1f64a4b97bd2cd951f4d0d25f2eaff176193906
samsongy <sam.l.songy@gmail.com>
2021-08-17 12:23:07 -0500

    Install ejs, set it as view engine for app.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({extended: true}));
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
