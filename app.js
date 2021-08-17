dffd425e0273bf5a525d44fc017d68a8decfba0b
samsongy <sam.l.songy@gmail.com>
2021-08-17 12:35:14 -0500

    Create header and footer partials. Create login page. Create get method to view login page.

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 
+
+app.get('/', (req, res) => {
+    res.render("login");
+});
+
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
