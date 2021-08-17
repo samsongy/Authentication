cb7ddd2a94ddd9adc4afda83362f54e2935841d9
samsongy <sam.l.songy@gmail.com>
2021-08-17 13:04:52 -0500

    Install mysql2 and dotenv packages. Establish connection to local mysql database in app.js. Create gitignore file and populate with node template from github.

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+require('dotenv').config();
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 
+var db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+  });
+
+  db.connect((err) => {
+      if(err) {
+          console.log(err);
+      } else {
+          console.log("Connected!")
+      }
+  });
 
 app.get('/', (req, res) => {
     res.render("login");
