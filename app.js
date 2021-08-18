a0ac769c7cbdc49a13e7e5b21ce078419b842aab
samsongy <sam.l.songy@gmail.com>
2021-08-17 20:43:10 -0500

    Install bcrypt package. Add post method for creating a new user. Hash and add salt to user password in user post method.

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
-var db = mysql.createConnection({
+const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
@@ -29,28 +32,57 @@ app.get('/', (req, res) => {
 });
 
 //REST API
+app.post('/api/users', (req, res) => {
+    //hash password entered, add salt
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        //create new user object, populate prooperties w/ form values
+        const user = {
+            FirstName:req.body.firstname,
+            LastName: req.body.lastname,
+            Username: req.body.username,
+            Email: req.body.email,
+            //send hashed password
+            Password: hash
+        }
+        //define sql query to create new user
+        let sql = 'INSERT INTO users SET ?';
+        //add user object to database
+        db.query(sql, user, (err, result, fields) => {
+            if(err) {
+                console.log(err);
+                res.redirect('/');
+            } else {
+                res.send("User successfully created!");
+            }
+        });
+    });
+});
+
+
+//For Testing Only
 app.get('/api/users', (req, res) => {
     let sql = "SELECT * FROM users";
-    db.query(sql, (err, rows, fields) => {
+    db.query(sql, (err, results, fields) => {
         if(err) {
             console.log(err);
             res.redirect('/');
         } else {
-            console.log(rows);
-            res.send(rows);
+            console.log(results);
+            res.send(results);
         }
     });
 });
 
+//For Testing Only
 app.get('/api/users/:id', (req, res) => {
     let sql = `SELECT * FROM users WHERE id = ${req.params.id}` 
-    db.query(sql, (err, rows, fields) => {
+    db.query(sql, (err, results, fields) => {
         if(err) {
             console.log(err);
             res.redirect('/');
         } else {
-            console.log(rows);
-            res.send(rows);
+            console.log(results);
+            res.send(results);
         }
     });
 });
